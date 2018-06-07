2cf71a594892e87ef15340390f038a2a51baa9be
RayChaung <a0919176442@gmail.com>
2018-06-07 23:38:22 +0800

    add receipt

--- app/src/main/java/iaccounting/csie/com/iaccounting/MainActivity.java
+++ app/src/main/java/iaccounting/csie/com/iaccounting/MainActivity.java
@@ -1,5 +1,9 @@
 package iaccounting.csie.com.iaccounting;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,9 +16,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends Drawer{
-
+    public ReceiptDB rdb = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,5 +50,32 @@ public class MainActivity extends Drawer{
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        rdb = new ReceiptDB(this);
+        add("1");
+        add("2");
+        add("3");
+
+    }
+
+    private void add(String s) {
+        SQLiteDatabase db = rdb.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put("_title",s.toString());//寫入資料
+        db.insert("TB1",null,values);
+        ListView LV1 = (ListView)findViewById(R.id.LV); //讀取元件
+        Cursor cursor = db.query("TB1",new String[]{"_id","_title"},null,null,null,null,null);
+        List<Map<String,Object>> items = new ArrayList<Map<String,Object>>();
+        cursor.moveToFirst();
+
+        for(int i = 0;i < cursor.getCount();i++) {
+            Map<String,Object> item = new HashMap<String,Object>();
+            item.put("_id",cursor.getString(0));
+            item.put("_title",cursor.getString(1));
+            items.add(item);
+            cursor.moveToNext();
+        }
+        SimpleAdapter SA = new SimpleAdapter(this,items,android.R.layout.simple_expandable_list_item_2,new String[]{"_id","_title"},new int[]{android.R.id.text1,android.R.id.text2});
+        LV1.setAdapter(SA);
     }
 }
